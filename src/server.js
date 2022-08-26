9f49d6b61e5b519d696c50ba975cb335c3596749
Kelvin Revertis <kelvinrev2@hotmail.com>
2022-08-26 08:43:46 -0300

    API to deploy

--- src/server.js
+++ src/server.js
@@ -11,5 +11,5 @@ server.use(bodyParser.urlencoded({extended: false}));
 server.use('/api', routes);
 
 server.listen(process.env.PORT,()=>{
-    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
+    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}/api/usuarios`);
 });
